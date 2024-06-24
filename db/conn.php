f17765e7c7b8ff11dafb7e844f727f629b65cb27
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-06-24 12:56:49 +0100

    finally figured outhow to set up MySQL database conection using mySQLi

--- db/conn.php
+++ db/conn.php
@@ -1,3 +1,19 @@
 <?php
+// Database configuration
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'iprevail');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'contact_db');
 
-echo "Hello World";
+// Setup MySQL server connection
+$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if ($mysqli->connect_error) {
+    die("ERROR: Could not connect. " . $mysqli->connect_error);
+}
+
+echo "Connected successfully";
+
+// Close connection
+$mysqli->close();
