0a5d3012063df31a49a2b79deaae8e797a49a91a
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-07-05 15:17:20 +0100

    index.php to retrieve all contacts in the dataBASE

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 <?php
+include("db/conn.php");
+$mysqli;
+
+// SQL command to retrieve all data in the database
+$sql = "SELECT * from contacts";
+
+// store the result in a variable
+$result = $mysqli->query($sql);
+// if there are existing rows in the database, display each row
+if(mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+        foreach($row as $key => $value) {
+            echo $key.":". " ".$value." ";
+    }
+    echo "<br>";
+    echo "<br>";
+}
+}
